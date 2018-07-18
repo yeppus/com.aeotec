e31203f1cc62df684ea0a63a68e4234e229d3c17
Matthew Waanders <matthew@athom.nl>
2018-07-18 11:26:05 +0200

    fixed zw120

--- drivers/ZW120/device.js
+++ drivers/ZW120/device.js
@@ -2,17 +2,15 @@
 
 const ZwaveDevice = require('homey-meshdriver').ZwaveDevice;
 
-class AeotecWaterSensorSixDevice extends ZwaveDevice {
+class AeotecContactSensorGenFiveDevice extends ZwaveDevice {
 	
 	onMeshInit() {
 		this.registerCapability('measure_battery', 'BATTERY');
 
-		this.registerCapability('alarm_tamper', 'NOTIFICATION');
-		this.registerCapability('alarm_water', 'NOTIFICATION');
-
-		this.registerCapability('measure_temperature', 'SENSOR_MULTILEVEL');
+        this.registerCapability('alarm_contact', 'SENSOR_BINARY');
+        this.registerCapability('alarm_tamper', 'NOTIFICATION');
 	}
 	
 }
 
-module.exports = AeotecWaterSensorSixDevice;
+module.exports = AeotecContactSensorGenFiveDevice;
