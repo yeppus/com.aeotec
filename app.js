7f05846475e1a7c3a72e397df63359b7ad376c2f
Matthew Waanders <matthew@athom.nl>
2019-01-04 12:54:06 +0100

    fixes

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ const Homey = require('homey');
 class AeotecApp extends Homey.App {
 	onInit() {
 		this.log(`${Homey.manifest.id} running...`);
+		let rainbowAction = new Homey.FlowCardAction('zw098_rainbow')
+            .register()
+            .registerRunListener((args, state) => {
+            	return args.device.rainbowModeHandler(args);
+			});
 	}
 }
 
